340809d17a632638dd928ac1cecabad587a875b2
ARGE\stsuellwold <stephan.suellwold@infa.de>
2019-12-12 09:23:06 +0100

    1.0.6 UploadQueueService has to be inherited by the host application. Therefore the @Injected() attribute has been removed. Register, Unregister and Find have been removed either, use specificly declared class members and enumerate them within getHandlers() instead.

--- src/upload-queue.service.ts
+++ src/upload-queue.service.ts
@@ -1,30 +1,23 @@
 import { SQLite } from '@ionic-native/sqlite/ngx';
 import { HttpClient } from '@angular/common/http';
-import { UploadQueue } from './upload-queue';
-import { IUploadQueue, IUploadQueueResponse } from './upload-queue.interfaces';
-import { JsonStorage } from 'infa-json-storage/dist/json-storage';
-import { Injectable } from '@angular/core';
+import { IUploadQueue } from './upload-queue.interfaces';
 
-@Injectable()
-export class UploadQueueService 
+export abstract class UploadQueueService
 {
     private timerId: number | undefined;
-    private handlers: Array<{ id: string, queue: IUploadQueue }>;
 
     constructor(
         protected sqlite: SQLite,
-        protected http: HttpClient) 
-    { 
-        this.handlers = new Array<{ id: string, queue: IUploadQueue }>();
-    }
+        protected http: HttpClient) { }
 
     public startService(interval?: number): void {
         if (this.timerId) {
             return;
         }
         this.timerId = setInterval(async () => {
-            for (let handler of this.handlers) {
-                await handler.queue.send();
+            const handlers: Array<IUploadQueue> = this.getHandlers();
+            for (let handler of handlers) {
+                await handler.send();
             }   
         }, interval ? interval : 10000);
     }
@@ -36,23 +29,5 @@ export class UploadQueueService
         clearInterval(this.timerId);
     }
 
-    public register<T, R extends IUploadQueueResponse>(id: string, handler: UploadQueue<T, R>) {
-        this.unregister(id);
-        this.handlers.push({ id, queue: handler });
-    }
-
-    public unregister(id: string) {
-        const idx = this.handlers.findIndex(x => x.id === id);
-        if (idx > -1) {
-            this.handlers.splice(idx, 1);
-        }
-    }
-
-    public findHandler<T>(id: string): (IUploadQueue & JsonStorage<T>) | undefined {
-        const item = this.handlers.find(x => x.id === id);
-        if (item) {
-            return item.queue as (IUploadQueue & JsonStorage<T>);
-        }
-        return undefined;
-    }
+    public abstract getHandlers(): Array<IUploadQueue>;
 }
